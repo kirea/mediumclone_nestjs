5d52ec1bdb2e280a28863c7b9a04f3a647cc0ed3
Eugene Kirvas <kirea.my@gmail.com>
2022-05-30 23:27:32 +0300

    follow user

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ProfileResponseInterface } from '@app/profile/types/profileResponse.interface';
 import { User } from '@app/user/decorators/user.decorator';
 import { ProfileService } from '@app/profile/profile.service';
+import { AuthGuard } from '@app/user/guards/auth.guard';
 
 @Controller('profiles')
 export class ProfileController {
@@ -14,6 +15,32 @@ export class ProfileController {
     @Param('username') profileUsername: string,
   ): Promise<ProfileResponseInterface> {
     const profile = await this.profileService.getProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.followProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Delete(':username/follow')
+  @UseGuards(AuthGuard)
+  async unfollowProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.unfollowProfile(
       currentUserId,
       profileUsername,
     );
