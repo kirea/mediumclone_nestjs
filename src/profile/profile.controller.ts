ee9695a460c02b0d91892af3c2e2a76d841a1ad5
Eugene Kirvas <kirea.my@gmail.com>
2022-05-30 22:36:10 +0300

    profile module

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProfileResponseInterface } from '@app/profile/types/profileResponse.interface';
+import { User } from '@app/user/decorators/user.decorator';
+import { ProfileService } from '@app/profile/profile.service';
+
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {
+  }
+
+  @Get(':username')
+  async getProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.getProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+}
